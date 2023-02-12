a5f6d05cd3a4484d683631de659b31f582e68c6a
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2023-02-12 22:28:22 +0700

    finish mega-nav

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,18 @@ for (let i = 0; i < navLinks.length; i++) {
         navLinks[i].classList.add("hovered");
         tempMenu.classList.add("open");
     })
+
     navLinks[i].addEventListener("mouseout", () => {
+        navLinks[i].classList.remove("hovered");
+        tempMenu.classList.remove("open");
+    })
+
+    tempMenu.addEventListener("mouseover", () => {
+        navLinks[i].classList.add("hovered");
+        tempMenu.classList.add("open");
+    })
+
+    tempMenu.addEventListener("mouseout", () => {
         navLinks[i].classList.remove("hovered");
         tempMenu.classList.remove("open");
     })
